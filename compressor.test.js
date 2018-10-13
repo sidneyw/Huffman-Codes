ec72a9b5d8ccfe5a6b65d56620fb5f25413a0365
Sidneyw <sidney.wijngaarde1@ibm.com>
2018-10-13 19:40:10 -0400

    Updates test case names

--- compressor.test.js
+++ compressor.test.js
@@ -5,13 +5,13 @@ const fileData = fs.readFileSync('./data/money.txt').toString();
 const fileCompressed = fs.readFileSync('./data/money.txt.huff').toString();
 
 describe('Compressor', () => {
-  test('Correctly encodes small text data', () => {
+  test('Correctly encodes data', () => {
     const { freqTable, encoded } = compressor.encode(fileData);
     expect(freqTable).toMatchSnapshot();
     expect(encoded).toMatchSnapshot();
   });
 
-  test('Correctly decodes small text data', () => {
+  test('Correctly decodes data', () => {
     const decoded = compressor.decode(fileCompressed);
     expect(decoded).toEqual(fileData);
   });
